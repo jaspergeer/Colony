5c37230f5b9943e93119981597583afb4db9c419
JasperGeer <jasper.geer@gmail.com>
2021-11-23 00:26:09 -0500

    Added icons

--- src/main/java/com/jaspergeer/app/ColonyApp.java
+++ src/main/java/com/jaspergeer/app/ColonyApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ColonyApp extends Application {
@@ -22,6 +23,7 @@ public class ColonyApp extends Application {
 
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/colony.png")));
         primaryStage.show();
     }
 }
